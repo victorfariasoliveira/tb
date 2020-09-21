17d1534d2db38a1bb58a110ea834d9c60af2bf6c
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2020-09-21 08:32:38 -0300

    feature: adding service methods to the controller

--- src/components/media/controller/media.controller.ts
+++ src/components/media/controller/media.controller.ts
@@ -1,32 +1,33 @@
 import { Controller, Get, Post, Body, Put, Delete, Param } from '@nestjs/common'
+import { Media } from '../interfaces/media.interface'
+import { MediaService } from '../services/media.service'
 
 @Controller('media')
 export class MediaController {
+  constructor(private readonly mediaService: MediaService) {}
+
   @Post()
-  async create(): Promise<any> {
-    return { info: 'created' }
+  async createOne(@Body() createMedia: Media): Promise<Media> {
+    return this.mediaService.create(createMedia)
   }
 
   @Get('all')
-  async findAll(): Promise<any> {
-    return { info: 'all' }
+  async findAll(): Promise<Media[]> {
+    return this.mediaService.findAll()
   }
 
   @Get(':id')
-  async findOne(@Param() params): Promise<string> {
-    return params.id
+  async findOne(@Param() params): Promise<Media> {
+    return this.mediaService.findOne(params.id)
   }
 
   @Put(':id')
-  async update(@Body() data: any, @Param() params): Promise<any> {
-    return {
-      id: params.id,
-      body: data,
-    }
+  async update(@Body() data: Media, @Param() params): Promise<Media> {
+    return this.mediaService.update(data, params.id)
   }
 
   @Delete(':id')
-  async delete(@Param() params): Promise<string> {
-    return params.id
+  async delete(@Param() params): Promise<void> {
+    this.mediaService.delete(params.id)
   }
 }
