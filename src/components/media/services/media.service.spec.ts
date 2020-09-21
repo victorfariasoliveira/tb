fe31a61d5667582200d5e5a250eec70bd197328f
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2020-09-21 07:37:13 -0300

    update: lean code

--- src/components/media/services/media.service.spec.ts
+++ src/components/media/services/media.service.spec.ts
@@ -2,18 +2,25 @@ import { Test, TestingModule } from '@nestjs/testing'
 import { Media } from '../interfaces/media.interface'
 import { MediaService } from './media.service'
 
-const mediaTest: Media = {
-  id: 1,
-  name: 'Luis Novo - Skore | K4: Conhecimento Líquido',
-  duration: 2219,
-  provider: 'jYuGqUaJiul0cB5Z',
-  media_type: 'video',
-  provider_id: 'youtube',
-  expires_at: 1603670400,
-}
-
 describe('MediaService', () => {
   let service: MediaService
+  const mediaTest: Media = {
+    id: 1,
+    name: 'Luis Novo - Skore | K4: Conhecimento Líquido',
+    duration: 2219,
+    provider: 'jYuGqUaJiul0cB5Z',
+    media_type: 'video',
+    provider_id: 'youtube',
+    expires_at: 1603670400,
+  }
+
+  let media1: Media
+  let media2: Media
+
+  beforeAll(async () => {
+    media1 = mediaTest
+    media2 = { ...mediaTest, id: 2, name: 'Framework Meta Learning | Skore' }
+  })
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -28,24 +35,20 @@ describe('MediaService', () => {
   })
 
   it('should create a media type', () => {
-    const mediaCreated = service.create(mediaTest)
-    expect(mediaCreated).toEqual(mediaTest)
+    const mediaCreated = service.create(media1)
+    expect(mediaCreated).toEqual(media1)
   })
 
   it('should obtain all the media created', () => {
-    const mediaCreated = service.create(mediaTest)
-    const mediaCreated2 = service.create({
-      ...mediaCreated,
-      id: 2,
-      name: 'Framework Meta Learning | Skore',
-    })
+    const mediaCreated = service.create(media1)
+    const mediaCreated2 = service.create(media2)
 
     const getAllMedias = service.findAll()
     expect(getAllMedias).toEqual([mediaCreated, mediaCreated2])
   })
 
   it('should update a media type', () => {
-    const mediaCreated = service.create(mediaTest)
+    const mediaCreated = service.create(media1)
     const mediaUpdate = { ...mediaCreated, name: 'Framework Meta Learning | Skore' }
     const mediaUpdated = service.update(mediaUpdate, '1')
     expect(mediaUpdated).toEqual(mediaUpdate)
@@ -53,7 +56,7 @@ describe('MediaService', () => {
 
   // TODO: fix
   it('should delete a media type', () => {
-    const mediaCreated = service.create(mediaTest)
+    const mediaCreated = service.create(media1)
     service.delete(mediaCreated)
 
     const arrysOfMedias = service.findAll()
