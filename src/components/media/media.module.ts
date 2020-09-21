3455d41a78c2bbb11d814d30d5de6e6eb20ba20c
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2020-09-20 23:59:13 -0300

    fix: conflit

--- src/components/media/media.module.ts
+++ src/components/media/media.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common'
+import { MediaController } from './controller/media.controller'
 import { MediaService } from './services/media.service'
 
 @Module({
   providers: [MediaService],
+  controllers: [MediaController],
 })
 export class MediaModule {}
