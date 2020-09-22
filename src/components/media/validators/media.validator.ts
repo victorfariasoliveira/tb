abc4e113a31d863574b058fb7c6a0b44e62590c0
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2020-09-22 13:36:42 -0300

    update: validation with new constants and addition of more input validators

--- src/components/media/validators/media.validator.ts
+++ src/components/media/validators/media.validator.ts
@@ -1,40 +1,55 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator'
+import { IsInt, IsNotEmpty, IsNumber, IsString, Length } from 'class-validator'
+
+const MEDIA_VALUES = {
+  name: [1, 80],
+  provider: [1, 10],
+  media_type: [1, 10],
+  provider_id: [1, 40],
+}
 
 export class PostMediaValidator {
-  @IsNumber()
+  @IsInt()
   readonly id: number
 
   @IsNotEmpty()
   @IsString()
+  @Length(MEDIA_VALUES.name[0], MEDIA_VALUES.name[1])
   readonly name: string
 
   @IsString()
+  @Length(MEDIA_VALUES.provider[0], MEDIA_VALUES.provider[1])
   readonly provider: string
 
   @IsNotEmpty()
+  @Length(MEDIA_VALUES.media_type[0], MEDIA_VALUES.media_type[1])
   readonly media_type: string
 
   @IsString()
+  @Length(MEDIA_VALUES.provider_id[0], MEDIA_VALUES.provider_id[1])
   readonly provider_id: string
 
-  @IsNumber()
+  @IsInt()
   readonly duration: number
 
-  @IsNumber()
+  @IsInt()
   readonly expires_at: number
 }
 
 export class PutMediaValidator {
   @IsString()
+  @Length(MEDIA_VALUES.name[0], MEDIA_VALUES.name[1])
   readonly name: string
 
   @IsString()
+  @Length(MEDIA_VALUES.provider[0], MEDIA_VALUES.provider[1])
   readonly provider: string
 
   @IsString()
+  @Length(MEDIA_VALUES.media_type[0], MEDIA_VALUES.media_type[1])
   readonly media_type: string
 
   @IsString()
+  @Length(MEDIA_VALUES.provider_id[0], MEDIA_VALUES.provider_id[1])
   readonly provider_id: string
 
   @IsNumber()
