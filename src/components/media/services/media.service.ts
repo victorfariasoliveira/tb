0e4c9126f3202c9837f3d6f342cef5a46813e211
victorfariasoliveira <victorfariasdeoliveira@gmail.com>
2020-09-22 22:39:58 -0300

    update: updating service media methods

--- src/components/media/services/media.service.ts
+++ src/components/media/services/media.service.ts
@@ -2,14 +2,17 @@ import { BadRequestException, Injectable } from '@nestjs/common'
 import { Media } from '../interfaces/media.interface'
 import { PostMediaValidator, PutMediaValidator } from '../validators/media.validator'
 import CONSTANTS from '../constants/media.constants'
+import MediaHelper from '../helpers/media.helper'
 
 @Injectable()
 export class MediaService {
-  private readonly medias: Media[] = []
+  private medias: Media[] = []
 
   create(media: PostMediaValidator): Media {
     try {
-      this.medias.push(media)
+      if (MediaHelper.duplicateIdChecker(this.medias, media.id)) throw new Error()
+
+      this.medias.push({ ...media, watched: false, expired: false })
       return this.medias.find(m => m.id == media.id)
     } catch (error) {
       throw new BadRequestException(CONSTANTS.badRequestCreate.message)
@@ -17,27 +20,44 @@ export class MediaService {
   }
 
   findAll(): Media[] {
-    return this.medias
+    return MediaHelper.mediaWithWatchedProperty(this.medias)
   }
 
-  findOne(id: number): Media {
-    const media = this.medias.find(m => m.id == id)
-    if (media === undefined) throw new BadRequestException(CONSTANTS.badRequestGet.message)
-    return media
+  findOne(id: string): Media {
+    try {
+      const media = this.medias.find(m => m.id == MediaHelper.parseId(id))
+      if (media === undefined) throw new Error()
+
+      MediaHelper.mediaWithExpiredProperty(media)
+      return { ...media, watched: true }
+    } catch (error) {
+      throw new BadRequestException(CONSTANTS.badRequestGet.message)
+    }
   }
 
   update(data: PutMediaValidator, id: string): Media {
     try {
-      const media = this.medias.find(m => m.id === parseInt(id))
-      return Object.assign(media, data)
+      let media = this.medias.find(m => m.id === MediaHelper.parseId(id))
+      if (media === undefined) {
+        throw new BadRequestException(CONSTANTS.badRequestGet.message)
+      }
+      media = { ...media, watched: false, expired: false }
+
+      this.medias = this.medias.map(m => (m.id === media.id ? Object.assign(media, data) : m))
+      return this.medias.find(m => m.id == media.id)
     } catch (error) {
+      console.log(error)
       throw new BadRequestException(CONSTANTS.badRequestUpdate.message)
     }
   }
 
-  delete(media: Media): void {
+  delete(id: string): void {
     try {
-      this.medias.splice(media.id, 1)
+      if (!MediaHelper.duplicateIdChecker(this.medias, MediaHelper.parseId(id))) throw new Error()
+      this.medias.splice(
+        this.medias.findIndex(m => m.id === MediaHelper.parseId(id)),
+        1,
+      )
     } catch (error) {
       throw new BadRequestException(CONSTANTS.badRequestDelete.message)
     }
